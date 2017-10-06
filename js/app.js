f0ee3beb80d2c2f97320ffa4b2db7460f59d3d42
RandyL <30959856+randyL78@users.noreply.github.com>
2017-10-06 18:30:40 -0400

    finished SASS conversion, began adding other sections

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		const size = window.innerWidth;
 		if (size < 768) {
 			return "small";
-		} else if (size < 1024) {
+		} else if (size < 1200) {
 			return "medium";
 		} else if (size < 1500) {
 			return "large";
@@ -112,7 +112,6 @@ document.addEventListener('DOMContentLoaded', function () {
 	// navigation functions for extra large screens
 	function navViewXLarge() { 
 		navViewLarge();
-//		main.style.marginLeft = "auto";
 	}
 
 	
@@ -156,8 +155,27 @@ document.addEventListener('DOMContentLoaded', function () {
 	const ctx = document.getElementById("line__traffic--Weekly").getContext('2d');
 	const ctx2 = document.getElementById("bar__traffic").getContext('2d');
 	const ctx3 = document.getElementById("pie__mobile").getContext('2d');
-	
-	
+	const trafficUL = document.getElementById("btns--traffic");
+	const hourlyLabels = [];
+	const oneWeekLabels = [];
+	const weeklyLabels = ['', '16-22', '23-29', '30-5', '6-12', '13-19', '20-26','27-3','4-10','11-17','18-24','25-31'];
+	const monthlyLabels = ["November",
+						  "December",
+						  "January",
+						  "Febraury",
+						  "March",
+						  "April",
+						  "May",
+						  "June",
+						  "July",
+						  "August",
+						  "September",
+						  "October"];
+	// generate 1 days worth of hourly data
+	const hourData = randomArray(24, 0, 18);
+	// generate 364 days worth of random data, will add random oneWeekData for last 7 days
+	const yearData = randomArray(358, 0, 375);
+
 	// ************* global settings
 	Chart.defaults.global.defaultFontColor = "#a4a4a4";
 	
@@ -179,6 +197,17 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 		return random;
 	}
+	
+	// populate labels variables
+	function fillLabels() {
+		for (let i = 1; i < 12; i++) {
+			hourlyLabels.push(i + ":00pm");
+		}
+		for (let i = 25; i <= 31; i++) {
+			oneWeekLabels.push(i);
+		}
+	}
+	
 	
 	// find the sum of an array of integers
 	function addArray(numbers) {
@@ -208,33 +237,63 @@ document.addEventListener('DOMContentLoaded', function () {
 		return weekly;
 	}
 	
-	// generate 1 days worth of hourly data
-	const hourData = randomArray(24, 0, 18);
+	function getMonthly(numberOfMonths) {
+		const month = [];
+		
+		for (let i = 0; i < numberOfMonths; i++) {
+			const days = arraySubset(yearData, 30, (i * 30));
+			month.unshift(addArray(days));
+		}
+		return month;
+	}
+	
+	// change data in traffic chart depending on which button is pressed
+	function changeTraffic(period) {
+		if (period.toLowerCase() === 'hourly') {
+			updateTraffic(hourlyLabels, hourData, 35);
+		} else if (period.toLowerCase() === 'daily') {
+			updateTraffic(oneWeekLabels, oneWeekData, 250);
+		} else if (period.toLowerCase() === 'weekly') {
+			updateTraffic(weeklyLabels, weeklyData, 2500);
+		} else if (period.toLowerCase() === 'monthly') {
+			updateTraffic(monthlyLabels, monthlyData, 8000);
+		}
+	}
+	
+	function updateTraffic(labels, data, max) {
+		trafficLine.data.labels = labels;
+		trafficLine.data.datasets[0].data = data;
+//		trafficLine.options.scales.yAxes[0].ticks.max = max;
+		trafficLine.update();
+	}
+	
+	
+
 	
 	// generate 1 week of data (including hour data)
 	const oneWeekData = randomArray(6,50,250);
 	oneWeekData.push(addArray(hourData));
 	
 	
-	// generate 364 days worth of random data, will add random oneWeekData for last 7 days
-	const yearData = randomArray(358, 0, 375);
-	// add data from oneWeekData to end of yearData
+		// add data from oneWeekData to end of yearData
 	for (let day of oneWeekData) {
 		yearData.push(day);
 	}
-		
+	
 	const weeklyData = getWeekly(12);
+	const monthlyData = getMonthly(12);
 	
 	const mobileData = [
 					randomNum(2000, 6000),
 					randomNum(2000, 6000),
 					randomNum(4000, 25000) // ensure desktop number remains larger
 	];
-
+	fillLabels();
 	
 	
 	
-	const weeklyTrafficLine = new Chart(ctx, {
+	
+	const trafficLine = new Chart(ctx, {
 		type: "line",
 		data: {
 			datasets: [{
@@ -247,7 +306,7 @@ document.addEventListener('DOMContentLoaded', function () {
 				pointBorderWidth: 2,
 				pointBackgroundColor: '#fff'
 			}],
-				labels: ['', '16-22', '23-29', '30-5', '6-12', '13-19', '20-26','27-3','4-10','11-17','18-24','25-31'],
+				labels: weeklyLabels,
     	},
 		options: {
 			responsive: true,
@@ -258,7 +317,7 @@ document.addEventListener('DOMContentLoaded', function () {
 				yAxes: [{
 					ticks: {
 						min: 0,
-						max: 2500
+//						max: 2500
 					}
 				}]
 			},
@@ -305,37 +364,119 @@ document.addEventListener('DOMContentLoaded', function () {
 	
 	
 var mobileUsersPie = new Chart(ctx3, {
-    type: 'doughnut',
-    data: {
-		datasets: [{
-			data: mobileData,
-			backgroundColor: ['#81c98f', "#74b1bf", '#7477BF'],
-			borderWidth: 0
-		}],
-		labels: ["Tablets", "Phones",  "Desktop"]
-	},
-	options: {
-		cutoutPercentage: 55,
-		legend: {
-		position: 'right',
-		labels: {
-			boxWidth: 20,
-			fontSize: 17
-			}
+		type: 'doughnut',
+		data: {
+			datasets: [{
+				data: mobileData,
+				backgroundColor: ['#81c98f', "#74b1bf", '#7477BF'],
+				borderWidth: 0
+			}],
+			labels: ["Tablets", "Phones",  "Desktop"]
 		},
-		layout: {
-			padding: 20
-		},
+		options: {
+			cutoutPercentage: 55,
+			legend: {
+			position: 'right',
+			labels: {
+				boxWidth: 20,
+				fontSize: 17
+				}
+			},
+			layout: {
+				padding: 20
+			},
 
-		rotation: (-0.65 * Math.PI)
+			rotation: (-0.65 * Math.PI)
+		}	
+	});
+	
+
+	// ************* Chart event listeners
+
+	trafficUL.addEventListener('click', function (e) {
+		const selected = trafficUL.querySelector(".selected");
+		const pressed = e.target;
+		if (pressed !== selected) {
+			selected.classList.remove("selected");
+			e.target.classList.add("selected");
+			changeTraffic(e.target.textContent);
+		}
+
+	 });
+
+// ************************************************************
+//				Members
+// ************************************************************
+
+	// =========== variables
+	const members = [];
+	const membersUL = document.getElementById("members");
+	const recentUL = document.getElementById("recent");
+	const imgURL = "./images/";
+	
+	
+	// =========== constructors
+	function Member(name, pic, email, date, activity, time) {
+		/*jshint validthis: true */
+		this.fullName = name;
+		this.avatar = pic;
+		this.email = email;
+		this.joinDate = date;
+		this.recentActivity = activity;
+		this.lastOn = time;
 	}
-});
 	
+	// ============ functions
 	
+	function createMemberArray (name, pic, email, date, activity, time) {
+		const member = new Member(name, pic, email, date, activity, time);
+		members.push(member);
+	}
+	
+	function createMemberArea () {
+		for (let member of members) {
+			const infoUL = createElement("ul", "social__stats");
+//			const 
+			infoUL.appendChild(createLI("", member.fullName));
+			
+			membersUL.appendChild(createProfileLI(member.avatar));	
+			membersUL.appendChild(infoUL);
+			
+		}
+	}
+	
+	function createProfileLI(img) {
+		const profileLI = createLI("");
+		const profileImg = createElement("img", "img--profile");
+		profileImg.setAttribute("src", imgURL + img + ".jpg");
+		profileLI.appendChild(profileImg);
+		return profileLI;
+	}
+	
+	function createLI (className, text = "") {
+		const li = createElement("li", className, text);
+
+		return li;
+	}
+	
+	function createElement (type, className, text = "") {
+		const el = document.createElement(type);
+		if (className !== "") {
+			el.classList.add(className);
+		}
+		if (text) {
+			el.textContent = text;
+		}
+		return el;
+	}
+	
+	// ============ main
+	
+	createMemberArray("Victoria Chambers", "65", "victoria.chambers80@example.com", "10/15/2017", "commented on YourApp's SEO Tips", "4 hours");
+	createMemberArray("Dale Byrd", "78", "dale.byrd52@example.com", "10/14/2017", "liked the Facebook's Changes for 2017", "5 hours");
+	
+	createMemberArea();
 });
-
-
-
 
 
 
